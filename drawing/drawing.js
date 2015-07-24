1b3670dc054ddbd89e5d6735d32dbbc914a3104a
Kleverson S. Paixão <Klev@users.noreply.github.com>
2015-07-24 18:34:36 -0400

    drawing js - multiple lines

--- drawing/drawing.js
+++ drawing/drawing.js
@@ -1,17 +1,27 @@
-﻿"use strict";
+"use strict";
 	
 var canvas;
 var gl;
 
-var bufferSize = 6000;
+var bufferSize = 1000000;
 var isDrawing;
-var lines;
 var line;
+var color;
+var colors;
+var colorOptions = [
+    vec4(1.0, 1.0, 1.0, 1.0),
+    vec4(1.0, 0.0, 0.0, 1.0),
+    vec4(0.0, 0.0, 1.0, 1.0),
+    vec4(0.0, 1.0, 0.0, 1.0),
+    vec4(1.0, 1.0, 0.0, 1.0),
+    vec4(0.0, 1.0, 1.0, 1.0),
+    vec4(1.0, 0.0, 1.0, 1.0)
+];
 
 window.onload = function init() 
 {
-	lines = [];
-	line = [];
+    line = [];
+    color;
 	isDrawing = false;
 	
 	canvas = document.getElementById("gl-canvas");
@@ -29,19 +39,26 @@ window.onload = function init()
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, bufferSize, gl.STATIC_DRAW);
-    
+
     var vPosition = gl.getAttribLocation( program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, bufferSize, gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
     
     canvas.addEventListener("mousedown", function(event) 
     	{
     		isDrawing = true;
-    		line = [];
     		var point = 
     			vec2( 2 * event.clientX / canvas.width - 1,
             	      2 * (canvas.height - event.clientY) / canvas.height - 1);
-           	line.push(point);
+    		line.push(point);
     	});
     
     canvas.addEventListener("mousemove", function(event)
@@ -51,31 +68,51 @@ window.onload = function init()
     			var point = 
     				vec2( 2 * event.clientX / canvas.width - 1,
             	          2 * (canvas.height - event.clientY) / canvas.height - 1);
-            	line.push(point);
-            	
-            	var index = 0;
-            	for (var i = 0; i < lines.length; i++)
-            	{
-            		gl.bufferSubData(gl.ARRAY_BUFFER, index, flatten(lines[i]));
-            		index += lines[i].length;
-            	}
-            	
-            	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(line));
+            		line.push(point);
+			    line.push(point);
+
+			    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(line));
     		}
     	});
     
     canvas.addEventListener("mouseup", function(event)
     	{
-    		isDrawing = false;
-            lines.push(line);
-            
-            var index = 0;
-            for (var i = 0; i < lines.length; i++)
-            {
-            	gl.bufferSubData(gl.ARRAY_BUFFER, index, flatten(lines[i]));
-            	index += lines[i].length;
-            }
-    	});
+    	    isDrawing = false;
+	        line.pop();
+        });
+
+    document.getElementById("clear").onclick = function (event)
+        {
+            line = [];
+    };
+
+    document.getElementById("white").onclick = function (event) {
+        color = colors[0];
+    };
+
+    document.getElementById("red").onclick = function (event) {
+        color = colors[1];
+    };
+
+    document.getElementById("blue").onclick = function (event) {
+        color = colors[2];
+    };
+
+    document.getElementById("green").onclick = function (event) {
+        color = colors[3];
+    };
+
+    document.getElementById("yellow").onclick = function (event) {
+        color = colors[4];
+    };
+
+    document.getElementById("cyan").onclick = function (event) {
+        color = colors[5];
+    };
+
+    document.getElementById("magenta").onclick = function (event) {
+        color = colors[6];
+    };
     	
     render();
 };
@@ -84,9 +121,8 @@ function render()
 {
 	gl.clear( gl.COLOR_BUFFER_BIT );
 	
-	for (var i = 0; i < lines.length; i++)
-		for (var j = 0; j < lines[i].length; j++)
-			gl.drawArrays(gl.LINE_STRIP, j, lines[i].length);
+	for (var i = 0; i < line.length - 1; i+=2)
+		gl.drawArrays(gl.LINES, i, 2);
 	
 	window.requestAnimFrame(render);
 }
